944a0998e383f9d264dfc40eca3ccc7b50b6430f
mikewheelmaker <radumihairotariu@gmail.com>
2021-02-17 13:36:04 +0200

    Chapter 2 Exercises

---
+++ Chapter2/mainCh2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	cout << "Exercise 2.5.7\n";
+	cout << "Write a C++ program that prints your name in the console window\n";
+	cout << "\n";
+	cout << "Mike\n";
+	cout << "\n";
+	cout << "Exercise 2.5.8\n";
+	cout << "Write a C++ program that prints your first and last name in the console window.\n";
+	cout << "Your first name should appear on one line, and your last name appear on the next line.\n";
+	cout << "\n";
+	cout << "Mike\n"
+		<< "Wheelmakersson\n";
+
+	return 0;
+}
