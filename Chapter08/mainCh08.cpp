0f1e41e02f14da046dfbce2bff0286a0cba1b34a
Mike Wheelmakersson <radumihairotariu@gmail.com>
2022-01-10 18:45:00 +0200

    Chapter 8 Exercises

---
+++ Chapter08/mainCh08.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << " Exercise 8.7.9\n";
+	cout << " From geometry: Write a computer program that given the lengths \n"
+		<< " of the two sides of a right triangle adjacent to the right angle \n"
+		<< " computes the length of the hypotenuse of the triangle.\n";
+	cout << '\n';
+	{
+		double side1 = 0.0;
+		double side2 = 0.0;
+		double hypotenuse = 0.0;
+		cout << "Please give the lengths of the two sides of the triangle: ";
+		cin >> side1 >> side2;
+		hypotenuse = sqrt(pow(side1, 2.0) + pow(side2, 2.0));
+		cout << "The hypotenuse is of length: " << hypotenuse << '\n';
+	}
+	cout << '\n';
+}
